5825554d3e4b481cd4ce818053f3e2c6eb62d24f
Jose de Jesus Aguinon Fermin <jose.agunooon@gmail.com>
2024-10-14 21:00:49 -0600

    Rutas 14 de Octubre de 2024

--- src/app/formulario/ejemplo1/ejemplo1.component.ts
+++ src/app/formulario/ejemplo1/ejemplo1.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,  ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+ 
+interface Usuario{
+  nombre:string;
+  edad: number;
+  email:string;
+}
+ 
+ 
  
 @Component({
   selector: 'app-ejemplo1',
@@ -9,19 +18,51 @@ import { FormBuilder, FormGroup,  ReactiveFormsModule } from '@angular/forms';
   styles: ``
 })
 export class Ejemplo1Component implements OnInit {
-    formGroup!: FormGroup;
-    constructor(private readonly fb: FormBuilder){}
-    ngOnInit(): void {
-        this.formGroup = this.initForm();
-    }
-    initForm(): FormGroup{
-      return this.fb.group({
-        email:[''],
-        password:['']
-      })
-    }
-    onSubmit():void{
-      console.log('Form ->' , this.formGroup.value);
-    }
-}
  
+  formGroup!: FormGroup;
+ 
+  nombre:string="AguiBoy"
+ 
+  persona:Usuario={
+    nombre:'',
+    edad: 0,
+    email:''
+  }
+ 
+ 
+  constructor(private readonly fb: FormBuilder){}
+ 
+  ngOnInit(): void {
+    this.formGroup = this.initForm();
+  }
+  initForm(): FormGroup{
+    return this.fb.group({
+      nombre:[''],
+      email:[''],
+      edad:['']
+    })
+  }
+  onSubmit():void{
+    const {nombre, edad, email} = this.formGroup.value;
+    this.persona.nombre=nombre
+    this.persona.edad=edad
+    this.persona.email=email
+ 
+ 
+    //console.log('Form ->', this.formGroup.value);
+ 
+    //localStorage.setItem("nombre",this.nombre);
+ 
+    let personaJSON = JSON.stringify(this.persona);
+ 
+    localStorage.setItem("persona", personaJSON);
+  }
+  subImprime():void{
+    const usuarioGuardado = localStorage.getItem('persona');
+    if (usuarioGuardado) {
+      const usuarioRecuperado: Usuario = JSON.parse(usuarioGuardado);
+      this.persona=usuarioRecuperado;
+    }
+  }
+ 
+}
